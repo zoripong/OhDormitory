f43971e3bde5af0a25a2fead6ad853c5bad468db
zoripong <dbfl0406@e-mirim.hs.kr>
2018-05-24 09:24:19 +0900

    create flask app

--- server/ohdormitory/test.py
+++ server/ohdormitory/test.py
@@ -1,3 +1,10 @@
-import sys
+from flask import Flask
 
-print("Hello World!")
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return "Hello World"
+
+if __name__ == '__main__':
+   app.run()
