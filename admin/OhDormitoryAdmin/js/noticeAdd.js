222111c70d89c666843f9fb39a48fe125bcb4fd9
yuriLee <yurilee9696@gmail.com>
2018-05-26 20:50:47 +0900

    notcie alert 2

---
+++ admin/OhDormitoryAdmin/js/noticeAdd.js
@@ -0,0 +1,38 @@
+//체크옵션에 따라 세부사항
+    function changeNoticeKind() {
+      var select_notice = document.getElementById("select_notice");
+      var select_clean = document.getElementById("select_clean");
+      var select_sleep = document.getElementById("select_sleep");
+      var notice_kind = document.getElementById("notice_kind");
+      if (notice_kind.value == "공지사항") {
+        select_notice.style.display = "block";
+        select_clean.style.display = "none";
+        select_sleep.style.display = "none";
+        $("#notice_title").val("");
+        $("#notice_title").removeAttr('readonly');
+        $("#notice_title").css('border', '');
+        $("#bookmark").css('background-image','url(image/notice-bookmark.svg)');
+        $("#w_time, #d_time").datepicker("option", "disabled", false);
+      } else if (notice_kind.value == "청소구역") {
+        select_notice.style.display = "none";
+        select_clean.style.display = "block";
+        select_sleep.style.display = "none";
+        $("#notice_title").val("청소구역");
+        $("#notice_title").attr('readonly', 'readonly');
+        $("#notice_title").css('border', 'none');
+        $("#bookmark").css('background-image','url(image/clean-bookmark.svg)');
+        $("#w_time").attr('readonly', 'readonly');
+        //$("#w_time, #d_time").datepicker("option", "disabled", false);
+      } else if(notice_kind.value == "외박일지"){
+        select_notice.style.display = "none";
+        select_clean.style.display = "none";
+        select_sleep.style.display = "block";
+        $("#sleep_w_time, #sleep_d_time").datepicker();
+        $("#notice_title").val("외박일지 작성");
+        $("#notice_title").attr('readonly', 'readonly');
+        $("#notice_title").css('border', 'none');
+        $("#bookmark").css('background-image','url(image/out-bookmark.svg)');
+        $("#w_time, #d_time").attr('readonly', 'readonly');
+        //$("#w_time, #d_time").datepicker("option", "disabled", false);
+      }
+    }
