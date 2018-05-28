fb288f299499b2822cc27d7be248b0a0012fb628
zoripong <dbfl0406@e-mirim.hs.kr>
2018-05-28 20:00:00 +0900

    Done Score Section (select, insert, delete) and Search User Account

---
+++ admin/OhDormitoryAdmin/js/models.js
@@ -0,0 +1,7 @@
+function User(emirim_id, password, name, room_num, parent_phone) {
+    this.emirim_id = emirim_id;
+    this.password = password;
+    this.name = name;
+    this.room_num = room_num;
+    this.parent_phone = parent_phone;
+};
