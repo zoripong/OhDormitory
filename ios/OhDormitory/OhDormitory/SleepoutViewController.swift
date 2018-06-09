62ff7133ee26c0b56133a80ccda5c260654c9fb5
kimdoori <rlaenfl0228@gmail.com>
2018-06-09 22:08:41 +0900

    sleepout

--- ios/OhDormitory/OhDormitory/SleepoutViewController.swift
+++ ios/OhDormitory/OhDormitory/SleepoutViewController.swift
@@ -7,67 +7,134 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class SleepoutViewController: UIViewController {
-
+class SleepoutViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var recognizeLabel: UILabel!
+    
+    let imagePicker: UIImagePickerController! = UIImagePickerController()
+   
+    
     struct Return_code : Codable{
         let return_code : Int
         let return_message : String
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let params = ["username":"john", "password":"123456"] as Dictionary<String, String>
+        //print("SleepoutViewController가 시작되었어요!");
         
-        var request = URLRequest(url: URL(string: "http://54.203.113.95/getSleepoutList.php")!)
-        request.httpMethod = "POST"
-        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let session = URLSession.shared
-        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
-            // print(response!)
-            do {
-                
-                
-                var return_code = [String]()
-                
-                var return_message = [String]()
-                
-                
-                
-                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
-                
-                
-                if let return_data =  json["return_code"] as? [[String: Any]]{
-                    print(return_data);
-                    for return_data_item in return_data {
-                        
-                        return_code.append("\(String(describing: return_data_item["return_code"]!))" )
-                        
-                        return_message.append(return_data_item["return_message"] as! String)
-                        
-                        
-                    }
-                    print(return_code)
-                    print(return_message)
+        DispatchQueue.global(qos: .userInitiated).async {
+            // Download file or perform expensive task
+            
+            //요청할 url
+            var components = URLComponents(string: "http://54.203.113.95/getSleepoutRecord.php")
+            //요청변수
+            components?.queryItems = [
+                URLQueryItem(name: "userID", value: "test2"),
+                URLQueryItem(name: "parentPhone", value: "010"),
+            ]
+            
+            guard let url = components?.url else { return }
+            var request = URLRequest(url: url)
+            
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let session = URLSession.shared
+            let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+                // print(response!)
+                do {
                     
+                    let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                    DispatchQueue.main.async {
+                        
+                        
+                        if let return_data =  json["return_code"] as? [[String: Any]]{
+                            print("리턴 데이터 : ",return_data);
+                            for return_data_item in return_data {
+                                
+                                let return_code : Int = return_data_item["return_code"]! as! Int
+                                let return_message : String = return_data_item["return_message"] as! String
+                                if(return_code == 2){
+                                    self.getSleepoutInfo(json: json,message : return_message)
+                                }else{
+                                    self.setSleepoutInfo(message :return_message)
+                                }
+                            }//for
+                            
+                        }//return_data
+                        
+                    }//ui updating
                     
+                } catch {
+                    print("error")
                 }
-                
-            } catch {
-                print("error")
-            }
-        })
-        
-        task.resume()
+            })
+            
+            task.resume()
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
-  
-
+    func getSleepoutInfo(json : Dictionary<String, AnyObject>,message:String){
+        if let notice_data =  json["notice"] as? [[String: Any]]{
+            print("notice : ",notice_data)
+            var notice_data_item = notice_data[0]
+            
+            if let record_data =  json["record"] as? [[String: Any]]{
+                var record_data_item = record_data[0]
+                let recognize : String = record_data_item["recognize"]! as! String
+                
+                self.setSleepoutInfo(isRecognize: recognize,sleep_w_time: notice_data_item["sleep_w_time"] as! String,sleep_d_time: notice_data_item["sleep_d_time"] as! String)
+            }
+            
+        }
+    }
+    
+    func setSleepoutInfo(isRecognize : String, sleep_w_time:String, sleep_d_time:String){
+        
+        var recognize : String = ""
+        
+        if(isRecognize=="0"){
+            recognize="미인증"
+        }
+        else{
+            recognize="인증완료"
+        }
+        
+        // print(recognize)
+        // print(sleep_w_time)
+        // print(sleep_d_time)
+        
+        self.dateLabel.text=sleep_w_time+" ~ "+sleep_d_time
+        //TODO: 번호 넣기
+        self.phoneLabel.text="인증 연락처 : 01012341234"
+        self.messageLabel.text=""
+        self.recognizeLabel.text=recognize
+        
+        
+    }
+    
+    func setSleepoutInfo(message : String){
+        print(message)
+        self.dateLabel.text=""
+        self.phoneLabel.text=""
+        self.messageLabel.text=message
+        self.recognizeLabel.text=""
+        
+    }
+    
+    
 }
+
